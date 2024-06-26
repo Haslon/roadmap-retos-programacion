1a44b66180f45467819b8b66457db9d68578c90b
Haslon <76234082@certus.edu.pe>
2024-06-25 19:31:37 -0500

    #00-Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/GersonFavianArcentalesZavala.py
@@ -0,0 +1,31 @@
+# https://www.python.org/
+
+#De una linea
+
+"""DE 
+Multiples
+Lineas"""
+
+#VARIABLE
+
+variable = 1
+
+#CONSTANTE
+
+None
+
+#VARIAS VARIABLES
+
+letras = "letras"
+
+numero = 1
+
+bool = True
+
+lista=[1, 2, 3, 4]
+
+tupla = (1, 2, 3, 4, 5)
+
+persona = {"nombre": "90dread", "edad": 33}
+
+print("¡Hola, Python!")
